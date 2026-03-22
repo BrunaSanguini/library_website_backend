00a7cd1ea536cc296ee94fe450d4bf6c1fae472a
BrunaSanguini <brunasmguedes@gmail.com>
2026-03-22 10:33:45 -0300

    update: utilizando MapStruct

--- backend/src/main/java/com/library/backend/mapper/LivroMapper.java
+++ backend/src/main/java/com/library/backend/mapper/LivroMapper.java
@@ -4,54 +4,13 @@ import com.library.backend.dto.LivroDetalheDTO;
 import com.library.backend.dto.LivroRequestDTO;
 import com.library.backend.dto.LivroResumoDTO;
 import com.library.backend.model.Livro;
+import org.mapstruct.Mapper;
 
-@Mapper
-public class LivroMapper {
+@Mapper(componentModel = "spring")
+public interface LivroMapper {
 
-    public static Livro livroRequestDTOToLivro(LivroRequestDTO livroRequestDTO) {
-        Livro livro = new Livro();
-        livro.setTitulo(livroRequestDTO.getTitulo());
-        livro.setAutor(livroRequestDTO.getAutor());
-        livro.setEditora(livroRequestDTO.getEditora());
-        livro.setAno(livroRequestDTO.getAno());
-        livro.setDescricao(livroRequestDTO.getDescricao());
-        livro.setGeneros(livroRequestDTO.getGeneros());
-        livro.setIsbn(livroRequestDTO.getIsbn());
-        livro.setPaginas(livroRequestDTO.getPaginas());
-        livro.setSubgeneros(livroRequestDTO.getSubgeneros());
+    Livro toEntity(LivroRequestDTO dto);
+    LivroResumoDTO toResumoDTO(Livro livro);
+    LivroDetalheDTO toDetalheDTO(Livro livro);
 
-        return livro;
-    }
-
-    public static LivroResumoDTO livroToLivroResumoDTO(Livro livro) {
-
-        LivroResumoDTO resumoDTO = new LivroResumoDTO();
-        resumoDTO.setAutor(livro.getAutor());
-        resumoDTO.setTitulo(livro.getTitulo());
-        resumoDTO.setEditora(livro.getEditora());
-        resumoDTO.setGeneros(livro.getGeneros());
-        resumoDTO.setId(livro.getId());
-
-        return resumoDTO;
-
-    }
-
-    public static LivroDetalheDTO livroToLivroDetalheDTO(Livro livro) {
-        LivroDetalheDTO detalheDTO = new LivroDetalheDTO();
-
-        detalheDTO.setAutor(livro.getAutor());
-        detalheDTO.setTitulo(livro.getTitulo());
-        detalheDTO.setEditora(livro.getEditora());
-        detalheDTO.setGeneros(livro.getGeneros());
-        detalheDTO.setAno(livro.getAno());
-        detalheDTO.setIsbn(livro.getIsbn());
-        detalheDTO.setPaginas(livro.getPaginas());
-        detalheDTO.setDescricao(livro.getDescricao());
-        detalheDTO.setSubgeneros(livro.getSubgeneros());
-        detalheDTO.setResenha(livro.getResenha());
-        detalheDTO.setId(livro.getId());
-
-        return detalheDTO;
-
-    }
 }
